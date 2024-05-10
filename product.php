0a503f48f70f1614ae5281384c95677666a67476
Boba Fett <marklloren169@gmail.com>
2024-05-10 23:51:29 +0800

    git

--- product.php
+++ product.php
@@ -13,13 +13,13 @@ session_start();
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <script src="https://kit.fontawesome.com/779f524f41.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <!-- Slick Carousel CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" />
 
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/product.css?=<?php echo time(); ?>">
 
 </head>
 
@@ -67,7 +67,7 @@ session_start();
         <div>
             <h2>Our Products</h2>
             <div class="prod-cat">
-                <nav id="prodcat">
+                <nav id="prodcat" class="prodcat">
                     <a href="?category=All">All</a>
                     <a href="?category=T-Shirt">T-Shirt</a>
                     <a href="?category=Jeans">Jeans</a>
@@ -80,6 +80,9 @@ session_start();
 
             <hr>
             <div class="all-prod" id="prodSlider">
+
+                <button onclick="scrollL()" class="sliderBtnleft"><i class="fa-solid fa-angles-left fa-4x"></i></button>
+                
                 <?php
                 include "connection.php";
 
@@ -123,16 +126,12 @@ session_start();
                 }
                 ?>
 
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination mt-5">
-                        <li class="page-item"><a class="page-link">Previous</a></li>
-                        <li class="page-item"><a class="page-link">1</a></li>
-                        <li class="page-item"><a class="page-link">2</a></li>
-                        <li class="page-item"><a class="page-link">Next</a></li>
-                    </ul>
-                </nav>
+                <button onclick="scrollR()" class="sliderBtnright"><i class="fa-solid fa-angles-right fa-4x" ></i> </button>
             </div>
     </section>
+
+  
+
 
     <!-- Banner -->
     <section id="banner">
@@ -152,6 +151,19 @@ session_start();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
 
     <script>
+         function scrollL(){
+            var aw = document.querySelector('.all-prod');
+            aw.scrollBy(-350,0)
+        }
+
+        function scrollR(){
+            var aw = document.querySelector('.all-prod');
+            aw.scrollBy(350,0)
+        }
+
+
+
+
         $('#categories .box-area').slick({
             slidesToShow: 3,
             slidesToScroll: 1,
