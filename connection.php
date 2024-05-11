b438f34e24eda9df39a97c43a25b792c72398634
Boba Fett <marklloren169@gmail.com>
2024-05-11 11:49:24 +0800

    dd

--- connection.php
+++ connection.php
@@ -1,1 +1,13 @@
 <?php
+$user = "maru";
+$pass = "1234";
+$host = "localhost";
+$conn = oci_connect($user, $pass, $host);
+
+if (!$conn) {
+    $error = oci_error();
+    echo "Connection failed: " . $error['message'];
+    exit;
+} else {
+    // echo "Connected to Oracle DB!<br>";
+}
